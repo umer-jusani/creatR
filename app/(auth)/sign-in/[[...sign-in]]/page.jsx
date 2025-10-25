9ef617a6d5cc63c2c25140211f7a77c5c5f1ea79
Umer Feroz <umer.jusani.04@gmail.com>
2025-10-25 05:51:52 +0500

    addanimation on the authentication pages.

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { SignIn } from '@clerk/nextjs'
+import Animation from '@/components/animation'
 
 const Signin = () => {
     return (
         <>
+            <Animation />
             <SignIn />
         </>
     )
--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { SignUp } from '@clerk/nextjs'
+import Animation from '@/components/animation'
 
 const Signup = () => {
     return (
         <>
+            <Animation />
             <SignUp />
         </>
     )
